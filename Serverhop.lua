a4e4e94823f0fd55c77e8ee4265519642e4917be
xaxa <80309202+xaxaxaxaxaxaxaxaxa@users.noreply.github.com>
2021-12-20 22:07:06 -0600

    Update Serverhop.lua

--- Serverhop.lua
+++ Serverhop.lua
@@ -1,1 +1,17 @@
+local HttpService, TeleportService = game.GetService(game, "HttpService"), game.GetService(game, "TeleportService");
+local JSONDecode, TeleportToPlaceInstance, format = HttpService.JSONDecode, TeleportService.TeleportToPlaceInstance, string.format;
+local OtherServers = JSONDecode(HttpService, game.HttpGet(game, format("https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Asc&limit=100", tostring(game.PlaceId))))["data"];
+local JobId, PlaceId = game.JobId, game.PlaceId;
 
+function Serverhop()
+    task.wait()
+    
+    for Index, Server in next, OtherServers do 
+        if JobId ~= Server.id then 
+            if Server.ping < 100 and Server.maxPlayers ~= Server.playing then 
+                TeleportToPlaceInstance(TeleportService, PlaceId, Server.id);
+                break 
+            end
+        end
+    end
+end; Serverhop()
